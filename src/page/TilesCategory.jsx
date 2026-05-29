18c43b8aeac6bad87f13262e14d5e963e381d67b
Sunny9467 <kumarsunny919200@gmail.com>
2026-05-29 10:47:27 +0530

    added

--- src/page/TilesCategory.jsx
+++ src/page/TilesCategory.jsx
@@ -139,6 +139,7 @@ export default function TilesCategory() {
       <Helmet>
         <title>{category.metatitle}</title>
         <meta name="description" content={category.metades} />
+        <link rel="canonical" href={category.cononical} />
       </Helmet>
 
       <img src={category.banner} className="w-full" alt={category.title} />
